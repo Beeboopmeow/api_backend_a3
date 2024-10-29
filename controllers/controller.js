cfdcb07daed4af4617de191c355b223c2311f6ab
Fabiano Raiser <fabiano.raiser16@gmail.com>
2024-10-29 18:46:19 -0300

    refactor: :recycle: Transfering the login function to CustomerController
    Transfering login function from Controller to CustomerController

--- controllers/controller.js
+++ controllers/controller.js
@@ -57,27 +57,6 @@ class Controller {
       next(e);
     }
   }
-
-  async login(req, res) {
-    try {
-      const { contact, password } = req.body;
-
-      const customer = await this.Model.findOne({
-        where: {
-          contact,
-          password,
-        },
-      });
-
-      if (customer) {
-        res.status(200).json({ message: 'Login bem-sucedido', customer });
-      } else {
-        res.status(401).json({ message: 'Contato ou senha incorretos'});
-      }
-    } catch (error) {
-      res.status(400).json({ error: error.message});
-    }
-  }
 }
 
 module.exports = Controller;
